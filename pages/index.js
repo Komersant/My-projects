bac9c3658dc67208f50a8cdec9f8d684e50b1125
Evgeniy Kushnir <kushnir.evgeniy2014@gmail.com>
2022-12-08 12:21:57 +0200

    add project my portfolio

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,22 @@ import {API_URL} from "../src/constants";
 import Home from "../src/components/screens/home/Home";
 
 export const getStaticProps = async () => {
-	const {data: links} = await axios.get(`${API_URL}/links`);
-	const {data: me} = await axios.get(`${API_URL}/me`);
-	const {data: socials} = await axios.get(`${API_URL}/socials`);
-	
-	return {
-		props: {
-			links, me, socials
+	try {
+		const {data: links} = await axios.get(`${API_URL}/links`);
+		const {data: me} = await axios.get(`${API_URL}/me`);
+		const {data: socials} = await axios.get(`${API_URL}/socials`);
+		
+		return {
+			props: {
+				links, me, socials
+			}
+		}
+	} catch (error) {
+		return {
+			notFound: true
 		}
 	}
+	
 	
 }
 

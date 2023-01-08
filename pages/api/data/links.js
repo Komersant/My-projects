f0a1fc6599b27ba771200123a768f99ddad4cdd7
Evgeniy Kushnir <kushnir.evgeniy2014@gmail.com>
2023-01-08 17:32:44 +0200

    add new project in my portfolio

--- pages/api/data/links.js
+++ pages/api/data/links.js
@@ -1,26 +1,32 @@
 export const links = [
-	{
-		_id: 1,
-		title: 'React Pizza',
-		icon: "/icons/pizza.svg",
-		link: 'https://komersant.github.io/React-pizza/',
-	},
-	{
-		_id: 2,
-		title: 'React To Do List',
-		icon: "/icons/to-do-list.svg",
-		link: 'https://komersant.github.io/React-To-Do-List/',
-	},
-	{
-		_id: 3,
-		title: 'React Quiz',
-		icon: "/icons/quiz.svg",
-		link: 'https://komersant.github.io/React-Quiz/',
-	},
-	{
-		_id: 4,
-		title: 'Frispes Landing',
-		icon: "/icons/landing.svg",
-		link: 'https://komersant.github.io/Frispes-Landing/',
-	}
-]
+  {
+    _id: 1,
+    title: 'React Pizza',
+    icon: 'https://cdn.icon-icons.com/icons2/1706/PNG/512/3986693-architecture-building-culture-pizza-pizzeria-restorant-icon_112267.png',
+    link: 'https://komersant.github.io/React-pizza/',
+  },
+  {
+    _id: 2,
+    title: 'React To Do List',
+    icon: 'https://cdn.icon-icons.com/icons2/1860/PNG/512/checklist_118086.png',
+    link: 'https://komersant.github.io/React-To-Do-List/',
+  },
+  {
+    _id: 3,
+    title: 'React Quiz',
+    icon: 'https://cdn.icon-icons.com/icons2/2898/PNG/512/test_exam_icon_182954.png',
+    link: 'https://komersant.github.io/React-Quiz/',
+  },
+  {
+    _id: 4,
+    title: 'Frispes Landing',
+    icon: 'https://cdn.icon-icons.com/icons2/560/PNG/512/Exit-Page_icon-icons.com_53732.png',
+    link: 'https://komersant.github.io/Frispes-Landing/',
+  },
+  {
+    _id: 5,
+    title: 'Online-kinotatr',
+    icon: 'https://cdn.icon-icons.com/icons2/41/PNG/128/movies_theater_media_7139.png',
+    link: 'https://online-kinoteatr.vercel.app/',
+  },
+];
--- pages/api/data/socials.js
+++ pages/api/data/socials.js
@@ -1,20 +1,26 @@
 export const socials = [
-	{
-		_id: 1,
-		title: 'GitHub',
-		icon: '/icons/github.svg',
-		link: 'https://github.com/Komersant'
-	},
-	{
-		_id: 2,
-		title: 'LinkedIn ',
-		icon: '/icons/linkedln.svg',
-		link: 'https://www.linkedin.com/in/yevhen-kushnir-a9a996247/'
-	},
-	{
-		_id: 3,
-		title: 'Telegram ',
-		icon: '/icons/telegram.svg',
-		link: 'https://t.me/Komersant1990'
-	}
-]
+  {
+    _id: 1,
+    title: 'GitHub',
+    icon: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/github_original_wordmark_logo_icon_146506.png',
+    link: 'https://github.com/Komersant',
+  },
+  {
+    _id: 2,
+    title: 'Vercel',
+    icon: 'https://seeklogo.com/images/V/vercel-logo-11E85AE911-seeklogo.com.png',
+    link: 'https://vercel.com/komersant',
+  },
+  {
+    _id: 3,
+    title: 'LinkedIn',
+    icon: 'https://img.icons8.com/color/512/linkedin-2.png',
+    link: 'https://www.linkedin.com/in/yevhen-kushnir-a9a996247/',
+  },
+  {
+    _id: 4,
+    title: 'Telegram',
+    icon: 'https://cdn.icon-icons.com/icons2/923/PNG/512/telegram_icon-icons.com_72055.png',
+    link: 'https://t.me/Komersant1990',
+  },
+];

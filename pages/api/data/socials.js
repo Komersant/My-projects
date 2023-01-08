c31e5fe86227727c50c21c87b34e9348c1693c41
Evgeniy Kushnir <kushnir.evgeniy2014@gmail.com>
2023-01-08 18:17:11 +0200

    change link and add new project

--- pages/api/data/socials.js
+++ pages/api/data/socials.js
@@ -2,25 +2,25 @@ export const socials = [
   {
     _id: 1,
     title: 'GitHub',
-    icon: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/github_original_wordmark_logo_icon_146506.png',
+    icon: '/icons/github.svg',
     link: 'https://github.com/Komersant',
   },
   {
     _id: 2,
     title: 'Vercel',
-    icon: 'https://seeklogo.com/images/V/vercel-logo-11E85AE911-seeklogo.com.png',
-    link: 'https://vercel.com/komersant',
+    icon: '/icons/vercel.svg',
+    link: 'https://vercel.com/komersant/',
   },
   {
     _id: 3,
-    title: 'LinkedIn',
-    icon: 'https://img.icons8.com/color/512/linkedin-2.png',
+    title: 'LinkedIn ',
+    icon: '/icons/linkedln.svg',
     link: 'https://www.linkedin.com/in/yevhen-kushnir-a9a996247/',
   },
   {
     _id: 4,
-    title: 'Telegram',
-    icon: 'https://cdn.icon-icons.com/icons2/923/PNG/512/telegram_icon-icons.com_72055.png',
+    title: 'Telegram ',
+    icon: '/icons/telegram.svg',
     link: 'https://t.me/Komersant1990',
   },
 ];
